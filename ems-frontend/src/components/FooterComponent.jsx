b30a6424eacc46781b311c696e9892f3f1d18424
David Bucio-Paz <daviddbp.buciopaz@gmail.com>
2025-09-03 04:59:38 -0400

    finished frontend for full stack app

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <p className='words'><span> All Rights Reserved 2025 by Davbp24 </span></p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
